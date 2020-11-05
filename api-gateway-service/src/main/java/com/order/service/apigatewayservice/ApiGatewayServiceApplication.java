733a4938fe3441431c93dad5ba9d60ecfdcc91b4
Aishwarya_aishu <aishubalaji4398@gmail.com>
2020-11-05 19:59:52 +0530

    Add files via upload

---
+++ api-gateway-service/src/main/java/com/order/service/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,19 @@
+package com.order.service.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@RefreshScope
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
